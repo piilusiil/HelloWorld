2183cfae3f9df0d71d6ec188f2e6b9d0904e2d73
fsnv877 <fsnv877>
2016-10-01 19:36:44 +0200

    Initial commit

---
+++ src/main/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.helloworld;
+
+/**
+ * Created by fsnv877 on 01/10/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Tšau <3");
+    }
+}
